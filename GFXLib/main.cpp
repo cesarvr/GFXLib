9ba9d16da44fce2c4d0d9105f610d55d1ce02620
kobe84 <cesarv01@gmail.com>
2014-05-04 16:09:39 +0200

    Initial Commit

---
+++ GFXLib/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GFXLib
+//
+//  Created by Cesar Luis Valdez on 04/05/14.
+//  Copyright (c) 2014 Cesar Luis Valdez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
