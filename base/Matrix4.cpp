37e416e739101265bd6e801f52bb404f33a84da3
kobe84 <cesarv01@gmail.com>
2014-08-07 01:11:14 +0200

    clean up & basic Matrix4

---
+++ base/Matrix4.cpp
@@ -0,0 +1,9 @@
+//
+//  Matrix4.cpp
+//  Block_saga
+//
+//  Created by Cesar Luis Valdez on 06/08/14.
+//  Copyright (c) 2014 Cesar Luis Valdez. All rights reserved.
+//
+
+#include "Matrix4.h"
---
+++ base/Matrix4.h
@@ -0,0 +1,14 @@
+//
+//  Matrix4.h
+//  Block_saga
+//
+//  Created by Cesar Luis Valdez on 06/08/14.
+//  Copyright (c) 2014 Cesar Luis Valdez. All rights reserved.
+//
+
+#ifndef __Block_saga__Matrix4__
+#define __Block_saga__Matrix4__
+
+#include <iostream>
+
+#endif /* defined(__Block_saga__Matrix4__) */
